59911ddc83b3cb6c99da1c0c835ef9de2ef62b38
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2011-12-31 00:41:05 +0100

    Administration page is working

--- src/main/scala/net/brosbit4u/snippet/Administration.scala
+++ src/main/scala/net/brosbit4u/snippet/Administration.scala
@@ -1,6 +1,6 @@
 
 
-package net.brosbit4u {  package snippet {
+package net.brosbit4u.snippet
 
 import java.util.Date
 import _root_.net.liftweb.util._
@@ -17,21 +17,21 @@ import Helpers._
 
 
 class Administration {
-    
+
     def usersList() = {
         val users = User.findAll.filter(u => u.level.is != 0)
         "tbody" #> users.map(u => {
-                    <tr><td>{u.lastName.is}</td><td>{u.firstName.is}</td>
+                <tr><td>{u.lastName.is}</td><td>{u.firstName.is}</td><td>{u.email.is}</td>
                     <td>{u.level.is.toString}</td><td>{u.karma.is.toString}</td></tr>
-            }) 
+            })
     }
-    
+
     def usersForm() = {
         var lastName = ""
         var firstName = ""
         var email = ""
-        var level = ""
-        
+        var level = 9
+
         def deleteData(){
             val users = User.findAll(By(User.email,email)).filter(u => u.level.is != 0)
             if(users.nonEmpty){
@@ -40,46 +40,46 @@ class Administration {
                     })
             }
         }
-        
+
         def saveData() {
             val users = User.findAll(By(User.email,email)).filter(u => u.level.is != 0)
             if(users.nonEmpty){
-               val u = users.head
-               u.firstName(firstName).lastName(lastName).email(email).level(level.toInt).save
+                val u = users.head
+                u.firstName(firstName).lastName(lastName).email(email).level(level.toInt).save
             }
             else {
                 val u = User.create
                 u.firstName(firstName).lastName(lastName).email(email).level(level.toInt).karma(0).save
             }
         }
-        
-        "#usersFormLastName" #> SHtml.text(lastName, lastName = _) &
-        "#usersFormFirstName" #> SHtml.text(firstName, firstName = _) &
-        "#usersFormEmail" #> SHtml.text(email, email = _) &
-        "#usersFormLevel" #> SHtml.text(level,level = _) &
-        "#usersFormSubmit"  #> SHtml.submit("Zapisz",saveData) &
-        "#usersFormDelete" #> SHtml.submit("Usuń",deleteData)        
+        val levels = (1 to 9).map(i => (i, i.toString))
+        "#usersFormLastName" #> SHtml.text(lastName, lastName = _,"class"->"Name","maxlenght"->"40") &
+        "#usersFormFirstName" #> SHtml.text(firstName, firstName = _,"class"->"Name","maxlenght"->"40") &
+        "#usersFormEmail" #> SHtml.text(email, email = _,"class"->"email","maxlenght"->"50") &
+        "#usersFormLevel" #> SHtml.selectObj[Int](levels, Full(level), level = _ ) &
+        "#usersFormSubmit"  #> SHtml.button(<img src="/images/saveico.png" />,saveData,"onclick"->"return isValid(this);") &
+        "#usersFormDelete" #> SHtml.button(<img src="/images/delico.png" />,deleteData)
     }
-    
+
     def departmentList() = {
         val depart = Department.findAll
         "tbody" #> depart.map(d => {
                 <tr><td>{d.name}</td><td>{d.short}</td><td>{d.subject}</td></tr>
             })
     }
-    
+
     def departmentForm() = {
         var name = ""
         var short = ""
         var subject = ""
-        
+
         def deleteData() {
             val departments = Department.findAll("short"-> short)
             if(departments.nonEmpty){
-               departments.head.delete
+                departments.head.delete
             }
         }
-        
+
         def saveData() {
             val departments = Department.findAll("short"-> short)
             if(departments.nonEmpty){
@@ -88,38 +88,39 @@ class Administration {
                 dep.short = short
                 dep.subject = subject
                 dep.save
-            } 
+            }
             else {
-                val dep = Department(new Date().getTime,name,short,subject)
+                val dep = Department(new Date().getTime,short,name, subject)
                 dep.save
             }
         }
-        
-        "#departmentFormName" #> SHtml.text(name, name = _) &
-        "#departmentFormShort" #> SHtml.text(short, short = _) &
-        "#departmentFormSubject" #> SHtml.text(subject, subject = _) &
-        "#departmentFormSubmit"  #> SHtml.submit("Zapisz",saveData) &
-        "#departmentFormDelete" #> SHtml.submit("Usuń",deleteData)
+        val subjects = Subject.findAll.map(s =>(s.short,s.full))
+        "#departmentFormName" #> SHtml.text(name, name = _,"class"->"name","maxlenght"->"20") &
+        "#departmentFormShort" #> SHtml.text(short, short = _,"class"->"name","maxlenght"->"9") &
+        "#departmentFormSubject" #> SHtml.select(subjects, Full(subjects.head._2),subject = _) &
+        "#departmentFormSubmit"  #> SHtml.button(<img src="/images/saveico.png" />,saveData,"onclick"->"return isValid(this);") &
+        "#departmentFormDelete" #> SHtml.button(<img src="/images/delico.png" />,deleteData)
     }
-    
+
     def subjectList() = {
         val subject = Subject.findAll
         "tbody" #> subject.map(s => {
-               <tr><td>{s.short}</td><td>{s.full}</td></tr>
+                <tr><td>{s.full}</td><td>{s.short}</td></tr>
             })
     }
-    
+
     def subjectForm() = {
         var short = ""
         var full = ""
-        
+
+
         def deleteData() {
             val subjects = Subject.findAll("short"-> short)
             if(subjects.nonEmpty){
                 subjects.head.delete
             }
         }
-        
+
         def saveData() {
             val subjects = Subject.findAll("short"-> short)
             if(subjects.nonEmpty){
@@ -127,20 +128,20 @@ class Administration {
                 sub.full = full
                 sub.short = short
                 sub.save
-            } 
+            }
             else {
                 val sub = Subject((new Date()).getTime, short, full)
                 sub.save
             }
         }
-        
-        "#subjectFormName" #> SHtml.text(full, full = _) &
-        "#subjectFormShort" #> SHtml.text(short, short = _) &
-        "#subjectFormSubmit"  #> SHtml.submit("Zapisz",saveData) &
-        "#subjectFormDelete" #> SHtml.submit("Usuń",deleteData)
-        
+
+        "#subjectFormName" #> SHtml.text(full, full = _,"class"->"name","maxlenght"->"30") &
+        "#subjectFormShort" #> SHtml.text(short, short = _,"class"->"name","maxlenght"->"9") &
+        "#subjectFormSubmit"  #> SHtml.button(<img src="/images/saveico.png" />,saveData,"onclick"->"return isValid(this);") &
+        "#subjectFormDelete" #> SHtml.button(<img src="/images/delico.png" />,deleteData)
+
     }
-    
+
     def dataAdmin() = {
         var email = ""
         var password = ""
@@ -156,14 +157,14 @@ class Administration {
                 admin.email(email).password(password).save
             }
         }
-            
-        "#adminPassword" #> SHtml.text(password, password = _,"type"->"password") &
-        "#adminEmail" #> SHtml.text(email, email = _) &
-        "#adminSubmit" #> SHtml.submit("", saveData,"type"->"image","src"->"/images/saveico.png","onclick"->"return isAdminValid();")
-        
+
+        "#adminPassword" #> SHtml.text(password, password = _,"type"->"password","class"->"password","maxlenght"->"20") &
+        "#adminEmail" #> SHtml.text(email, email = _,"class"->"required email","maxlenght"->"50","class"->"email") &
+        "#adminSubmit" #> SHtml.button(<span>ZAPISZ! <img src="/images/saveico.png" /></span>, saveData,"onclick"->"return isValid(this);")
+
     }
-    
+
 }
 
-}}
+
 
